923094940812820d743b9ce960c1243dc81704b4
Agustinus Adam <agustinusadamw@gmail.com>
2020-04-19 02:55:54 +0700

    school-application-sequelize (19/04/2020) update routing & controllers untuk data teachers (belum ejs untuk teachers)

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -1,40 +1,49 @@
 const {Student} = require('../models');
 
 class StudentsController {
+    // Menampilkan seluruh data dari database
     static showStudents(req, res) {
         Student.findAll()
-            .then(data => {
-                let pesan = req.query.pesan
-                let id = req.params.id
-                res.render("students.ejs", {data, pesan, id})
-            })
-            .catch(err => {
-                res.send(err)
-            })
+        .then(data => {
+            let pesan = req.query.pesan
+            let id = req.params.id
+            res.render("students.ejs", {data, pesan, id})
+        })
+        .catch(err => {
+            res.send(err)
+        })
     }
 
+    // Routing / Menampilkan form add new student
     static getAddForm(req, res) {
         let error = req.query.error
         res.render("add-students.ejs", {error})
     }
 
+
+    // Mengolah input user dari form add new student untuk kemudian 
+    // ditambilkan kembali pada halaman utama students data
     static postAdd(req, res) {
         let fn = false;
         let ln = false;
         let eml = false;
         let bd = false;
+        
+        // Check & Validasi First Name
         if (req.body.first_name === '' || req.body.first_name === undefined) {
             res.redirect('/students/add?error=First Name harus diisi')
         } else {
             fn = true
         }    
         
+        // Check & Validasi Last Name
         if (req.body.last_name === '' || req.body.last_name === undefined) {
             res.redirect('/students/add?error=Last Name harus diisi')
         } else {
             ln = true
         }   
         
+        // Check & Validasi email
         if (req.body.email === '' || req.body.email === undefined) {
             res.redirect('/students/add?error=Email harus diisi')
         } else if (req.body.email) {
@@ -44,7 +53,8 @@ class StudentsController {
                 res.redirect('/students/add?error=Email harus ada character @')
             }
         }
-    
+
+        // Check & Validasi Birth Date
         if (req.body.birth_date === '' || req.body.birth_date === undefined) {
             res.redirect('/students/add?error=Birth Date harus diisi')
         } else if (req.body.birth_date) {
@@ -81,6 +91,7 @@ class StudentsController {
             }
         }
 
+        // Check All validations
         if (fn && ln && eml && bd) {
             let queryBody = req.body
             Student.create({
@@ -99,36 +110,44 @@ class StudentsController {
         } 
     }
 
+    // Routing atau menampilkan form edit students
     static getEditForm(req, res) {
         let id = req.params.id
         Student.findByPk(id)
-            .then(data => {
-                let dataStudents = data.dataValues
-                let error = req.query.error                
-                res.render('edit-students.ejs', {id, dataStudents, error})
-            })
-            .catch(err => {
-                res.send(err)
-            })
+        .then(data => {
+            let dataStudents = data.dataValues
+            let error = req.query.error                
+            res.render('edit-students.ejs', {id, dataStudents, error})
+        })
+        .catch(err => {
+            res.send(err)
+        })
     }
 
+    // Mengolah input user dari form edit students untuk kemudian 
+    // ditambilkan kembali pada halaman utama students data dengan data yang telah
+    // terupdate
     static postEdit(req, res) {
         let fn = false;
         let ln = false;
         let eml = false;
         let bd = false;
+
+        // Check & validasi first name
         if (req.body.first_name === '' || req.body.first_name === undefined) {
             res.redirect(`/students/edit/${req.params.id}?error=First Name harus diisi`)
         } else {
             fn = true
         }    
         
+        // Check & validasi last name
         if (req.body.last_name === '' || req.body.last_name === undefined) {
             res.redirect(`/students/edit/${req.params.id}?error=Last Name harus diisi`)
         } else {
             ln = true
         }   
         
+        // check & validasi email
         if (req.body.email === '' || req.body.email === undefined) {
             res.redirect(`/students/edit/${req.params.id}?error=Email harus diisi`)
         } else if (req.body.email) {
@@ -138,7 +157,8 @@ class StudentsController {
                 res.redirect(`/students/edit/${req.params.id}?error=Email harus ada character @`)
             }
         }
-    
+
+        // check & validasi birth date
         if (req.body.birth_date === '' || req.body.birth_date === undefined) {
             res.redirect(`/students/add?error=Birth Date harus diisi`)
         } else if (req.body.birth_date) {
@@ -175,6 +195,7 @@ class StudentsController {
             }
         }
 
+        // Check All validations
         if (fn && ln && eml && bd) {
             let queryBody = req.body
             let id = req.params.id
@@ -189,47 +210,49 @@ class StudentsController {
                 res.redirect(`/students?pesan=berhasil edit data student dengan id ${id}`)
             })
             .catch(err => {
-                res.send()
+                res.send(err)
             })
         }
     }
 
+    // Delete data sesuai dengan id yang dipilih
     static delete(req, res) {
         let id = req.params.id
         Student.destroy({where: {id}})
-            .then(data => {
-                res.redirect(`/students?pesan=berhasil delete data student dengan id ${id}`)
-            })
-            .catch(err => {
-                res.send(err)
-            })
+        .then(data => {
+            res.redirect(`/students?pesan=berhasil delete data student dengan id ${id}`)
+        })
+        .catch(err => {
+            res.send(err)
+        })
     }
 
+    // Filter student berdasarkan id
     static searchStudentsById(req, res) {
         Student.findAll()
-            .then(data => {
-                let dataById = []
-                let queryBody = req.body.students_by_id
-                let pesan = null
-                let check = false
-                for (let i in data) {
-                    if (data[i].id == queryBody) {
-                        dataById.push(data[i])
-                        check = true
-                    }
+        .then(data => {
+            let dataById = []
+            let queryBody = req.body.students_by_id
+            let pesan = null
+            let check = false
+            for (let i in data) {
+                if (data[i].id == queryBody) {
+                    dataById.push(data[i])
+                    check = true
                 }
+            }
 
-                if (check == false) {
-                    pesan = "Maaf data students dengan id " + queryBody + " tidak ada dalam database"
-                    res.render("student-by-id.ejs", {dataById, queryBody, pesan})
-                } else {
-                    pesan = ''
-                    res.render("student-by-id.ejs", {dataById, queryBody, pesan})
-                }
-            })
-            .catch(err => {
-                res.send(err)
-            })
+            if (check == false) {
+                pesan = "Maaf data students dengan id " + queryBody + " tidak ada dalam database"
+                res.render("student-by-id.ejs", {dataById, queryBody, pesan})
+            } else {
+                pesan = ''
+                res.render("student-by-id.ejs", {dataById, queryBody, pesan})
+            }
+        })
+        .catch(err => {
+            res.send(err)
+        })
     }
 }
 
--- routes/teachers-router.js
+++ routes/teachers-router.js
@@ -3,10 +3,10 @@ const TeachersController = require('../controllers/teachers-controller')
 
 router.get('/', TeachersController.showTeachers)
 router.get('/add', TeachersController.getAddForm)
-// router.post('/add', TeachersController.postAdd)
+router.post('/add', TeachersController.postAdd)
 router.get('/edit/:id', TeachersController.getEditForm)
-// router.post('/edit/:id', TeachersController.postEdit)
-// router.get('/delete/:id', TeachersController.delete)
-// router.post('/:email', TeachersController.searchTeachersByEmail)
+router.post('/edit/:id', TeachersController.postEdit)
+router.get('/delete/:id', TeachersController.delete)
+router.post('/:email', TeachersController.searchTeacherByEmail)
 
 module.exports = router
