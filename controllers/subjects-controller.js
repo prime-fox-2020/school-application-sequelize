299bc722548bc9ac454d39587db0e7a7e918e80b
Agustinus Adam <agustinusadamw@gmail.com>
2020-04-19 12:51:40 +0700

    school-application-sequelize (19/04/2020) update data subjects

--- controllers/subjects-controller.js
+++ controllers/subjects-controller.js
@@ -25,19 +25,31 @@ class SubjectsController {
     // ditambilkan kembali pada halaman utama students data
     static postAdd(req, res) {
         let sn = false;
+        let nm = false;
 
         // Check & Validasi Subject Name
         if (req.body.subject_name === '' || req.body.subject_name === undefined) {
             res.redirect('/subjects/add?error=Subject Name harus diisi')
         } else {
             sn = true
-        }    
+        }
+        
+        if (req.body.nilai_minimum === '' || req.body.nilai_minimum == undefined) {
+            res.redirect('/subjects/add?error=Nilai Minimum harus diisi')
+        } else if (req.body.nilai_minimum) {
+            if (req.body.nilai_minimum < 11 && req.body.nilai_minimum > 0) {
+                nm = true
+            } else {
+                res.redirect('/subjects/add?error=Nilai Minimum tidak valid')
+            }
+        }
         
         // Check All validations
-        if (sn) {
+        if (sn && nm) {
             let queryBody = req.body
             Subject.create({
-                "subject_name": queryBody.subject_name
+                "subject_name": queryBody.subject_name,
+                "nilai_minimum": req.body.nilai_minimum
             })
             .then(data => {
                 res.redirect(`/subjects?pesan=berhasil menambah data subject dengan nama ${queryBody.subject_name}`)
@@ -67,20 +79,32 @@ class SubjectsController {
     // terupdate
     static postEdit(req, res) {
         let sn = false;
-    
+        let nm = false;
+
         // Check & validasi subject name
         if (req.body.subject_name === '' || req.body.subject_name === undefined) {
             res.redirect(`/subjects/edit/${req.params.id}?error=Subject Name harus diisi`)
         } else {
             sn = true
-        }    
+        } 
+        
+        if (req.body.nilai_minimum === '' || req.body.nilai_minimum === undefined) {
+            res.redirect(`/subjects/edit/${req.params.id}?error=Nilai Minimum harus diisi`)
+        } else if (req.body.nilai_minimum) {
+            if (req.body.nilai_minimum < 11 && req.body.nilai_minimum > 0) {
+                nm = true
+            } else {
+                res.redirect(`/subjects/edit/${req.params.id}?error=Nilai Minimum tidak valid`)
+            }
+        }
         
         // Check All validations
-        if (sn) {
+        if (sn && nm) {
             let queryBody = req.body
             let id = req.params.id
             Subject.update({
-                "subject_name": queryBody.subject_name
+                "subject_name": queryBody.subject_name,
+                "nilai_minimum": req.body.nilai_minimum
             }, {returning: true, where: {id}})
             .then(data => {
                 res.redirect(`/subjects?pesan=berhasil edit data subject dengan id ${id}`)
